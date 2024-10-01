5b0a292f98c28ec8490eefc2831b6ac944103167
Théo Lebègue <theo.lebegue@icloud.com>
2024-10-01 16:40:50 +0200

    feat(comment): adding the comment in the readme

--- xpeho_ui_android/src/main/java/com/xpeho/xpeho_ui_android/FilePreviewButton.kt
+++ xpeho_ui_android/src/main/java/com/xpeho/xpeho_ui_android/FilePreviewButton.kt
@@ -48,7 +48,6 @@ import com.xpeho.xpeho_ui_android.foundations.Fonts
 enum class LabelPosition {
     TOP, BOTTOM
 }
-
 @Composable
 fun FilePreviewButton(
     labelStart: String = "File Preview Button",
@@ -97,115 +96,132 @@ fun FilePreviewButton(
         label = ""
     )
 
-    Column(
+    Box(
         modifier = Modifier
             .clip(RoundedCornerShape(12.dp))
-            .background(if (enabled) backgroundColor else Color.Gray.copy(alpha = 0.3f))
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        pressed = true // Set pressed state
-                        onPress() // Call onPress action
-
-                        // Wait until the press is released
-                        tryAwaitRelease()
-                        pressed = false // Reset pressed state when released
-                    }
-                )
-            }
+            .background(backgroundColor)
     ) {
-        if (labelPosition == LabelPosition.TOP) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp)
-                    .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = labelStart,
-                    fontSize = labelSize,
-                    color = labelColor,
-                    fontFamily = Fonts.raleway
-                )
-                Text(
-                    text = labelEnd,
-                    fontSize = labelSize,
-                    color = labelColor,
-                    fontFamily = Fonts.raleway
-                )
-            }
-        }
-
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(height)
+                .pointerInput(Unit) {
+                    if (enabled) {
+                        detectTapGestures(
+                            onPress = {
+
+                                pressed = true
+                                onPress()
+
+                                tryAwaitRelease()
+                                pressed = false
+
+                            }
+                        )
+                    }
+                }
         ) {
-            imagePreview()
+            if (labelPosition == LabelPosition.TOP) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                        .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = labelStart,
+                        fontSize = labelSize,
+                        color = labelColor,
+                        fontFamily = Fonts.raleway
+                    )
+                    Text(
+                        text = labelEnd,
+                        fontSize = labelSize,
+                        color = labelColor,
+                        fontFamily = Fonts.raleway
+                    )
+                }
+            }
+
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.3f)
-                    .align(Alignment.BottomCenter)
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black.copy(alpha = 0.65f)
-                            ),
-                        )
-                    )
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomStart)
-                    .horizontalScroll(rememberScrollState())
-                    .clipToBounds()
-                    .padding(10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .height(height)
             ) {
-                Row(modifier = Modifier.weight(1f)) {
-                    tags.forEach { tag ->
-                        Box(modifier = Modifier.padding(horizontal = 4.dp)) {
-                            tag()
+                imagePreview()
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.3f)
+                        .align(Alignment.BottomCenter)
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Colors.CONTENT_COLOR
+                                ),
+                            )
+                        )
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomStart)
+                        .horizontalScroll(rememberScrollState())
+                        .clipToBounds()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row(modifier = Modifier.weight(1f)) {
+                        tags.forEach { tag ->
+                            Box(modifier = Modifier.padding(horizontal = 4.dp)) {
+                                tag()
+                            }
                         }
                     }
-                }
 
-                // Arrow Icon with animation
-                arrowIcon(
-                    Modifier
-                        .offset(x = arrowOffset.dp)
-                        .alpha(arrowAlpha)
-                )
+                    // Arrow Icon with animation
+                    arrowIcon(
+                        Modifier
+                            .offset(x = arrowOffset.dp)
+                            .alpha(arrowAlpha)
+                    )
+                }
+            }
+
+            if (labelPosition == LabelPosition.BOTTOM) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = labelStart,
+                        fontSize = labelSize,
+                        color = labelColor,
+                        fontFamily = Fonts.raleway
+                    )
+                    Text(
+                        text = labelEnd,
+                        fontSize = labelSize,
+                        color = labelColor,
+                        fontFamily = Fonts.raleway
+                    )
+                }
             }
         }
 
-        if (labelPosition == LabelPosition.BOTTOM) {
-            Row(
+        // Gray overlay when disabled
+        if (!enabled) {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = labelStart,
-                    fontSize = labelSize,
-                    color = Color.Black,
-                    fontFamily = Fonts.raleway
-                )
-                Text(
-                    text = labelEnd,
-                    fontSize = labelSize,
-                    color = Color.Black,
-                    fontFamily = Fonts.raleway
-                )
-            }
+                    .matchParentSize()
+                    .background(Colors.DISABLED_COLOR.copy(alpha = 0.65f))
+            )
         }
     }
 }
+
 
 @Preview
 @Composable
@@ -216,6 +232,7 @@ fun FilePreviewButtonPreview() {
         Box(modifier = Modifier.padding(16.dp)) {
             Column {
                 FilePreviewButton(
+                    height = 200.dp,
                     tags = listOf(
                         { TagPill("TAG PILL 1", backgroundColor = Colors.XPEHO_COLOR) },
                         { TagPill("TAG PILL 2", backgroundColor = Colors.XPEHO_COLOR) },
@@ -225,17 +242,39 @@ fun FilePreviewButtonPreview() {
                     labelPosition = LabelPosition.TOP,
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-
                 FilePreviewButton(
+                    height = 200.dp,
+                    labelStart = "File Preview Button Customized",
                     tags = listOf(
-                        { TagPill("TAG PILL CUSTOM 1", backgroundColor = Colors.XPEHO_COLOR) },
-                        { TagPill("TAG PILL CUSTOM 2", backgroundColor = Colors.XPEHO_COLOR) },
-                        { TagPill("TAG PILL CUSTOM 3", backgroundColor = Colors.XPEHO_COLOR) },
-                        { TagPill("TAG PILL CUSTOM 4", backgroundColor = Colors.XPEHO_COLOR) },
+                        { TagPill("TAG PILL CUSTOM 1", backgroundColor = Colors.RED_INFO_COLOR) },
+                        { TagPill("TAG PILL CUSTOM 2", backgroundColor = Colors.RED_INFO_COLOR) },
+                        { TagPill("TAG PILL CUSTOM 3", backgroundColor = Colors.RED_INFO_COLOR) },
+                        { TagPill("TAG PILL CUSTOM 4", backgroundColor = Colors.RED_INFO_COLOR) },
                     ),
                     labelColor = Color.White,
-                    backgroundColor = Color.Black,
+                    backgroundColor = Colors.CONTENT_COLOR,
                     labelPosition = LabelPosition.BOTTOM,
+                    arrowIcon =  { modifier ->
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrow_right),
+                            contentDescription = "Arrow icon",
+                            tint = Colors.RED_INFO_COLOR,
+                            modifier = modifier
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                FilePreviewButton(
+                    enabled = false,
+                    labelStart = "File Preview Button Disabled",
+                    height = 200.dp,
+                    tags = listOf(
+                        { TagPill("TAG PILL 1", backgroundColor = Colors.XPEHO_COLOR) },
+                        { TagPill("TAG PILL 2", backgroundColor = Colors.XPEHO_COLOR) },
+                        { TagPill("TAG PILL 3", backgroundColor = Colors.XPEHO_COLOR) },
+                        { TagPill("TAG PILL 4", backgroundColor = Colors.XPEHO_COLOR) },
+                    ),
+                    labelPosition = LabelPosition.TOP,
                 )
             }
         }
